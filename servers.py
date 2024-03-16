a3a6a82d60d3ee7238ba790ebb9640d0e467b90a
Den <no@>
2024-03-16 21:39:35 +0300

    рекламная кампания

--- servers.py
+++ servers.py
@@ -38,5 +38,44 @@ def image_mars():
                 </html>
                '''
 
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                        integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                        crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/mars_planet.jpeg')}" 
+                        alt="здесь должна была быть картинка, но не нашлась">
+                        Вот она какая, красная планета.
+                        <div class="bg-warning text-dark">
+                          Человечество вырастает из детства.
+                        </div>
+                        <div class="bg-success text-dark">
+                          Человечеству мала одна планета.
+                        </div>
+                        <div class="bg-info text-dark">
+                           Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="bg-secondary text-dark">
+                          И начнем с Марса!
+                        </div>
+                        <div class='bg-success text-light'>
+                          Присоединяйся!
+                        </div>
+                        <div class='bg-info text-muted'>
+                          Для отключения рекламы оформите подписку :)
+                        </div>
+                      </body>
+                    </html>"""
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
