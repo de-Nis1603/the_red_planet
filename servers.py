fedfdbc5a798657bf138dabb7bc055263449a622
Den <no@>
2024-03-18 00:07:37 +0300

    пейзажи Марса

--- servers.py
+++ servers.py
@@ -198,5 +198,55 @@ def astronaut_selection():
         print(request.form['file'])
         print(request.form['accept'])
         return 'Форма отправлена, вылет завтра:)'
+
+
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                                    <html lang="en">
+                                      <head>
+                                        <meta charset="utf-8">
+                                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                                        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+                                        rel="stylesheet"
+                                        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+                                        crossorigin="anonymous">
+                                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/form_style.css')}" />
+                                        <title>Пейзажи Марса</title>
+                                      </head>
+                                      <body>
+                                        <h1>Пейзажи Марса</h1>
+                                        <div>
+                                          <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" data-bs-interval=5000>
+                                            <div class="carousel-inner" role='listbox'>
+                                              <div class="carousel-item active">
+                                                <img src="{url_for('static', filename='img/mars_planet.jpeg')}" class="d-block w-100" alt="">
+                                              </div>
+                                              <div class="carousel-item">
+                                                <img src="{url_for('static', filename='img/mars_scenery.jpg')}" class="d-block w-100" alt="">
+                                              </div>
+                                              <div class="carousel-item">
+                                                <img src="{url_for('static', filename='img/mars_surface.jpg')}" class="d-block w-100" alt="">
+                                              </div>
+                                              <div class="carousel-item">
+                                                <img src="{url_for('static', filename='img/the_core.jpg')}" class="d-block w-100" alt="">
+                                              </div>
+                                              </div>
+                                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Previous</span>
+                                              </button>
+                                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <span class="visually-hidden">Next</span>
+                                              </button>
+                                              </div>
+                                              <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+                                              integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+                                              crossorigin="anonymous"></script>
+                                            </form>
+                                        </div>
+                                      </body>
+                                    </html>'''
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
